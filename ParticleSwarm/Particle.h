3f42e0f7b7b3b48c52c0b1dde36f82d464cc6ce9
dneedell <dneedell@bowdoin.edu>
2015-03-01 23:50:03 -0500

    Coded header files for skeleton.
    I coded the header files and developed the skeleton for two out of the
    three classes.  I also included the PDF of the assignment in the folder
    which has specifics highlighted and detailing which group member does
    what.

--- ParticleSwarm/Particle.h
+++ ParticleSwarm/Particle.h
@@ -10,5 +10,48 @@
 #define __ParticleSwarm__Particle__
 
 #include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+//constant used to calculate new particle velocities
+const double phi1 = 2.05;
+
+class Particle {
+    
+public:
+    //constructor function
+    Particle();
+    
+    //getter functions
+    double getPosition(int dimNumber);
+    double getVelocity(int dimNumber);
+    double getAccel(int dimNumber);
+    double getPBest(int dimNumber);
+    double getPBestValue(){ return pBestValue; };
+    
+    //setter functions
+    void setPosition(int dimNumber, double posValue);
+    void setVelocity(int dimNumber, double velValue);
+    void setAccel(int dimNumber, double accelValue);
+    void setPBest(int dimNumber, double posValue);
+    void setPBestValue(double newPBestValue);
+    
+private:
+    //information about the particle's position as a vector
+    vector<double> position;
+    
+    //information about the particle's velocity as a vector
+    vector<double> velocity;
+    
+    //information about the particle's acceleration as a vector
+    vector<double> acceleration;
+    
+    //information about the personal best positions, accelerations, and values
+    vector<double> pBestPos;
+    vector<double> pBestAccel;
+    double pBestValue;
+    
+};
 
 #endif /* defined(__ParticleSwarm__Particle__) */
--- ParticleSwarm/Swarm.h
+++ ParticleSwarm/Swarm.h
@@ -10,5 +10,46 @@
 #define __ParticleSwarm__Swarm__
 
 #include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+//constants that will be used to calculate new best particle velocities
+const double phi2 = 2.05;
+const double constrictionFactor = .7298;
+
+class Swarm {
+    
+public:
+    //constructor function
+    Swarm(int numParticles);
+    
+    //getter functions
+    double getGBestPos(int dimNumber);
+    double getGBestAccel(int dimNumber);
+    double getBestValue(){ return gBestValue; };
+    double getNumParticles(){ return numParticles; };
+    
+    //setter functions
+    void setGBestPos(int dimNumber);
+    void setGBestAccel(int dimNumber);
+    void setGBestValue();
+    
+    //test functions
+    double eval(int functionNumber, vector<double> pos);
+    double evalRosenbrock(vector<double> pos);
+    double evalAckley(vector<double> pos);
+    double evalRastrigin(vector<double> pos);
+    
+private:
+    //global best information
+    vector<double> gBestPos;
+    vector<double> gBestAccel;
+    double gBestValue;
+    
+    //number of particles in swarm
+    int numParticles;
+    
+};
 
 #endif /* defined(__ParticleSwarm__Swarm__) */
